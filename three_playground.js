1a0562e8c7e46b68f8ae4aa4b518951df4a78d73
Sathvik Hegde <91815940+sathvik-hegde@users.noreply.github.com>
2022-04-10 19:22:43 +0800

    Added new essay, updated three playground
    Added bare bones essay to introduce three.js, and updated three js section in "projects" to verify whether three.js is working.

---
+++ three_playground.js
@@ -0,0 +1,35 @@
+import * as THREE from "../external_scripts/three.module.js";
+import { OrbitControls } from '../external_scripts/OrbitControls.js';
+import { GLTFLoader } from '../external_scripts/GLTFLoader.js';
+
+// console.log(THREE);
+const container = document.getElementById('scene_container');
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('skyblue');
+
+const fov = 35;
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1;
+const far = 100;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(0, 0 ,10);
+
+const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial();
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+container.append(renderer.domElement);
+renderer.render(scene, camera);
+
+
+  
+
+
+
+
+
